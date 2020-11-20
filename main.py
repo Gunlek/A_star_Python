905bfd4291610780212e3c57cb115dc894850b59
Fabien Aubret <fabien.aubret@gmail.com>
2020-11-20 11:29:07 +0100

    (Main) Add heuristic computing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import pygame
 import sys
 from cell import Cell
-from datetime import datetime
+import math
 
 pygame.init()
 
@@ -19,14 +19,17 @@ state_2_color = (39, 174, 96)   # Path
 state_3_color = (22, 160, 133)  # Start point
 state_4_color = (142, 68, 173)  # Target point
 state_5_color = (44, 62, 80)    # Wall
+state_6_color = (129, 236, 236) # Highlighted cells
 text_color = (243, 156, 18)
 
-size = WIDTH, HEIGHT = 500, 500
+size = WIDTH, HEIGHT = 720, 720
 
 screen = pygame.display.set_mode(size)
 grid_surface = pygame.surface.Surface(size)
 
 pygame.font.init()
+small_font_size = 15
+small_cell_font = pygame.font.SysFont('agencyfb', small_font_size)
 font = pygame.font.SysFont('agencyfb', 30)
 step_0_text = font.render('Cliquez pour définir le point de départ', True, text_color)
 step_1_text = font.render("Cliquez pour définir le point d'arrivée", True, text_color)
@@ -58,7 +61,7 @@ def draw_grid(surface, width, height, nb_x, nb_y):
     return grid
 
 # Grid size in number of cells per axis
-grid_size_x, grid_size_y = 20, 20
+grid_size_x, grid_size_y = 15, 15
 # Cell size in pixels
 cell_size_x, cell_size_y = WIDTH // grid_size_x, HEIGHT // grid_size_y
 grid = draw_grid(grid_surface, WIDTH, HEIGHT, grid_size_x, grid_size_y)
@@ -81,10 +84,11 @@ def draw_states(surface, grid, width, height):
                 pygame.draw.rect(surface, state_4_color, pygame.Rect(cell.getX() * cell_size_x, cell.getY() * cell_size_y, cell_size_x, cell_size_y))
             if cell.getState() == 5:
                 pygame.draw.rect(surface, state_5_color, pygame.Rect(cell.getX() * cell_size_x, cell.getY() * cell_size_y, cell_size_x, cell_size_y))
+            if cell.getState() == 6:
+                pygame.draw.rect(surface, state_6_color, pygame.Rect(cell.getX() * cell_size_x, cell.getY() * cell_size_y, cell_size_x, cell_size_y))
 
 # Handle screen update and refresh on each rendering cycle
-def screen_update():
-    screen.fill(bg_color)
+def screen_update(evaluated_neighbours=[]):
     draw_states(screen, grid, WIDTH, HEIGHT)
     # screen.blit(grid_surface, (0, 0))
 
@@ -98,9 +102,51 @@ def screen_update():
     elif startup_step == 3:
         screen.blit(step_3_text, (0, 0))
 
+    if len(evaluated_neighbours) > 0:
+        for neighbours in evaluated_neighbours:
+            start_dist = small_cell_font.render(str(neighbours['start_dist']), True, text_color)
+            end_dist = small_cell_font.render(str(neighbours['end_dist']), True, text_color)
+            heuristic = small_cell_font.render(str(neighbours['heuristic']), True, text_color)
+
+            screen.blit(start_dist, (neighbours['cell'].getX() * cell_size_x, neighbours['cell'].getY() * cell_size_y))
+            screen.blit(end_dist, (neighbours['cell'].getX() * cell_size_x, neighbours['cell'].getY() * cell_size_y + small_font_size))
+            screen.blit(heuristic, (neighbours['cell'].getX() * cell_size_x, neighbours['cell'].getY() * cell_size_y + small_font_size*2))
+
     pygame.display.flip()
 
+def get_neighbours(cell, grid):
+    cell_x = cell.getX()
+    cell_y = cell.getY()
+
+    neighbours = []
+
+    for x in range(cell_x-1, cell_x+2):
+        for y in range(cell_y-1, cell_y+2):
+            if x >= 0 and x < len(grid):
+                if y >= 0 and y < len(grid[x]):
+                    if not (x == cell_x and y == cell_y):
+                        if grid[x][y].getState() != 5:
+                            neighbours.append(grid[x][y])
+    
+    for cell in neighbours:
+        cell.setState(6)
+    return neighbours
+
+def evaluate_heuristic(cell, target_cell, departure_cell):
+    distance_at_startpoint = round(math.sqrt((cell.getX() - departure_cell.getX())**2 + (cell.getY() - departure_cell.getY())**2), 2)
+    distance_at_endpoint = round(math.sqrt((cell.getX() - target_cell.getX())**2 + (cell.getY() - target_cell.getY())**2), 2)
+
+    heuristic = distance_at_startpoint + distance_at_endpoint
+
+    return distance_at_startpoint, distance_at_endpoint, heuristic
+
+current_cell = grid[0][0]
+target_cell = grid[0][0]
+departure_cell = grid[0][0]
+
 while 1:
+    screen.fill(bg_color)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -114,12 +160,28 @@ while 1:
             
             if startup_step == 0:
                 grid[mouse_cell_x][mouse_cell_y].setState(3)
+                current_cell = grid[mouse_cell_x][mouse_cell_y]
+                departure_cell = grid[mouse_cell_x][mouse_cell_y]
                 startup_step += 1
             elif startup_step == 1:
                 grid[mouse_cell_x][mouse_cell_y].setState(4)
+                target_cell = grid[mouse_cell_x][mouse_cell_y]
                 startup_step += 1
             elif startup_step == 2:
                 grid[mouse_cell_x][mouse_cell_y].setState(5)
+        
+    evaluated_neighbours = []
+    if startup_step >= 4:
+        # The game is running, let's run the algorithm
+        neighbours = get_neighbours(current_cell, grid)
+        for cell in neighbours:
+            data = {}
+            start_dist, end_dist, heuristic = evaluate_heuristic(cell, target_cell, departure_cell)
+            data['cell'] = cell
+            data['start_dist'] = start_dist
+            data['end_dist'] = end_dist
+            data['heuristic'] = heuristic
+            evaluated_neighbours.append(data)
 
-    screen_update()
-    pygame.display.flip()
+
+    screen_update(evaluated_neighbours)
